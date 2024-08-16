206742ddb3c58d6b8b8d96016cbc0d06bb8d9fe0
Seweryn <smicho01@student.bbk.ac.uk>
2024-08-16 16:58:09 +0100

    Add docker compose

--- db/sql/001-create-person-table.sql
+++ db/sql/001-create-person-table.sql
@@ -1,4 +1,9 @@
-CREATE TABLE IF NOT EXISTS person (
+CREATE TABLE IF NOT EXISTS demoapi.person (
     id VARCHAR(36)  PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+INSERT INTO demoapi.person (id, name) VALUES
+    ('ac99dbf1-3f6c-4fc9-801e-aeb666e723da', 'John Doe'),
+    ('74b45ccb-57d8-4f04-be86-ff9423ea411a', 'Adam Smith'),
+    ('3f093674-a368-4a4c-b79f-dc2c4d5c9c35', 'Anna Fleming');
